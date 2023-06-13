0f6c95215fec6dd79fc1c26b2464376093c988c5
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-06-13 23:26:45 +0600

    array & hashing: arraySign

---
+++ array & hashing/arraySign.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function (nums) {
+  function signFunc(x) {
+    if (x > 0) {
+      return 1
+    } else if (x < 0) {
+      return -1
+    }
+    return 0
+  }
+
+  let product = 1
+
+  for (let num of nums) {
+    product *= num
+  }
+
+  return signFunc(product)
+}
+
+console.log(arraySign([-1, -2, -3, -4, 3, 2, 1]))
