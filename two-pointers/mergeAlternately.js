1eb153bd25e7b21aec391a1f9a98f5c496e55d74
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-06-13 23:36:25 +0600

    two-pointers: mergeAlternately

---
+++ two-pointers/mergeAlternately.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+  let ans = []
+
+  let i = 0
+  let j = 0
+
+  while (i < word1.length && j < word2.length) {
+    ans.push(word1[i++], word2[j++])
+  }
+
+  while (i < word1.length) {
+    ans.push(word1[i++])
+  }
+
+  while (j < word2.length) {
+    ans.push(word2[j++])
+  }
+
+  return ans.join("")
+}
+
+console.log(mergeAlternately("abc", "pqrst"))
